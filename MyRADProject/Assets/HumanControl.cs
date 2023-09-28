a5e4d3bb09d9d5060296df0ae8c042b9d3a0983e
STUDENT Vladyslav Proskurin <Vladyslav.Proskurin@students.ittralee.ie>
2023-09-28 10:59:58 +0100

    Dublication
    Signed-off-by: STUDENT Vladyslav Proskurin <Vladyslav.Proskurin@students.ittralee.ie>

---
+++ MyRADProject/Assets/HumanControl.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanControl : MonoBehaviour
+{
+    float currentSpeed, walkingSpeed = 2, runningSpeed = 4;
+    private float turningSpeed = 180;
+    Animator myAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSpeed = walkingSpeed;
+        myAnimator= GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myAnimator.SetBool("walk", false);
+        if (Input.GetKey(KeyCode.W))
+        {
+            myAnimator.SetBool("walk", true);
+            transform.position += currentSpeed * transform.forward * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            myAnimator.SetBool("walk", true);
+            transform.position -= currentSpeed * transform.forward * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up, turningSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.up, turningSpeed * Time.deltaTime);
+        }
+    }
+}
